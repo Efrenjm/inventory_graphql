55fdfe1733ce9b31ca2c0806a2516f90532e49f0
Efren Alejandro Jimenez Mukul <efrenjimenez_96@hotmail.com>
2024-07-25 14:54:58 -0600

    :bug: fix CORS with frontend

--- src/main/java/com/demo_graphql/controller/ItemController.java
+++ src/main/java/com/demo_graphql/controller/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class ItemController {
